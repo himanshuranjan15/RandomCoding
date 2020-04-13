babdc19930446bc2698baffa785e0db12441ae6d
himanshuranjan15 <63094055+himanshuranjan15@users.noreply.github.com>
2020-04-13 11:00:14 +0530

    Update 4.Migratory Birds.py

--- 4.Migratory Birds.py
+++ 4.Migratory Birds.py
@@ -1,5 +1,5 @@
 def migratoryBirds(arr):
-    x=dict([[l,arr.count(l)] for l in set(arr)])
+    x=dict([[l,arr.count(l)] for l in set(arr)]) #counting elements in a list a saving it as dictionary
     z=max(x.values())
     y=[]
     for i in x:
